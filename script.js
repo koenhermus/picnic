5a319f304e9d00b950a5c9e1f5e8d386fa0fa4f4
koenhermus <160845989+koenhermus@users.noreply.github.com>
2024-10-30 19:47:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+student_rect.append("div")
+.style("width", "100px")
+.style("height", "100px")
+.style("float", "left")
+.style("border", "1px solid black")
+.html("<img src='/img/HubLayout.png' alt='Map' width='1000' height='1000'");
